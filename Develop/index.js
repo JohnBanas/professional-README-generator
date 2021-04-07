97ce61d08a9f2649e3a08966a6f93b345281df98
JPB <jbanas9124@gmail.com>
2021-04-06 21:08:00 -0500

    created node functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: ''
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
