e75a145634937f7960aca0d08f4f278d2233f538
JPB <jbanas9124@gmail.com>
2021-04-06 20:56:27 -0500

    fs require and inquirer require

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,15 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
