4123b4899fe2056e046500049de64887d7ac280d
JPB <jbanas9124@gmail.com>
2021-04-08 17:44:16 -0500

    fixed the generateMarkdown() to create desired dyntax in README.md file

--- README1.md
+++ README1.md
@@ -1,34 +1,46 @@
-# Example Title
+# Title here
 
-    ![badge](https://img.shields.io/badge/license-MIT License-brightorange)
+## Description
+
+Nunc mattis enim ut tellus elementum sagittis. Sit amet volutpat consequat mauris. Nunc mattis enim ut tellus elementum sagittis. Sit amet volutpat consequat mauris. 
+
+![badge](https://img.shields.io/badge/license-MITLicense-brightorange)
+
+You can access more badges and their purposes at [shields.io](https://shields.io)
+
+## Table of Contents
+  * [Contribution](#contribution)
+  * [Testing](#testing)
+  * [Contact](#contact)
+  * [License](#license)
     
-    You can access more badges and their purposes at [shields.io](https://shields.io)
+    
+      
+## Contribution
 
-    ## Description
+  _If you would like to contribute, please adhere to these guidelines:_
 
-    Example description lorem ipsum
-## **Table of Contents**
-  ---* [Installation](#installation)* [Usage](#usage)* [Contribution](#contribution)* [Testing](#testing)* [Questions](#questions)* [Contact](#contact)* [License](#license)
-    ---## Installation
-      ---
-      _Follow these steps to properly install this application:_
-      installation example ## Usage
-      ---
-      _Instructions for use:_
-      Here is how you use it ## Contribution
-      ---
-      _If you would like to contribute, please adhere to these guidelines:_
-      other developers contribution example## Testing
-      ---
-      _Instructions for testing application:_
-      Testing can be done by example## Questions
-      ---
-      _For further questions please follow these steps:_
-      This is an example of how to contact me## Contact
-      ---
-      _Contact Info:_
-      * GitHub: [JohnBanas](https://github.com/JohnBanas)
-      * Email: [example@email.com](example@email.com)## License
-      ---
-      This application has the MIT License. 
-      For more information please view the [license description](https://choosealicense.com/licenses/mit/).
+  Nunc mattis enim ut tellus elementum sagittis. Sit amet volutpat consequat mauris.
+      
+## Testing
+
+  _Instructions for testing application:_
+
+  Nunc mattis enim ut tellus elementum sagittis. Sit amet volutpat consequat mauris. Nunc mattis enim ut tellus elementum sagittis. Sit amet volutpat consequat mauris.
+    
+## Contact
+      
+  _Contact Info:_
+
+  * GitHub: [JohnBanas](https://github.com/JohnBanas)
+
+  * Email: [example@gmail.com](example@gmail.com)
+    
+## License
+
+      
+  _This application has the MIT License._
+      
+  For more information please view the [license description](https://choosealicense.com/licenses/mit/).
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,138 +1,187 @@
 // do not need license setup outside of generate markdown
 function generateMarkdown(data) {
-  //create variable to hold markdown template and dynamically add to it
-  let markdownTemplate =
-    //title, badge, and description
-    `# ${data.title}
-
-    ![badge](https://img.shields.io/badge/license-${data.license}-brightorange)
-    
-    You can access more badges and their purposes at [shields.io](https://shields.io)
-
-    ## Description
-
-    ${data.description}
-`;
-  //create optional sections in a table of contents to 
-  //be added after required title, description
-
-  let tableOfContents = `## **Table of Contents**
-  ---`;
-  if (data.installation) {
-    tableOfContents += `* [Installation](#installation)`
-  };
-  if (data.instructions) {
-    tableOfContents += `* [Usage](#usage)`
-  };
-  if (data.contribution) {
-    tableOfContents += `* [Contribution](#contribution)`
-  };
-  if (data.testing) {
-    tableOfContents += `* [Testing](#testing)`
-  };
-  if (data.questions) {
-    tableOfContents += `* [Questions](#questions)`
-  };
-
-  //append table to template
-  markdownTemplate += tableOfContents;
-  
-  //add contact info and license (required)
-  markdownTemplate +=
-    `* [Contact](#contact)`;
-  markdownTemplate +=
-    `* [License](#license)
-    ---`;
-  
-    //add optional sections content
-  
-  //installation
-  if (data.installation) {
-    markdownTemplate +=
-      `## Installation
-      ---
-      _Follow these steps to properly install this application:_
-      ${data.installation}`
-  };
-  
-  //usage
-  if (data.instructions) {
-    markdownTemplate +=
-      `## Usage
-      ---
-      _Instructions for use:_
-      ${data.instructions}`
-  };
-
-  //contributions
-  if (data.contribution) {
-    markdownTemplate +=
-      `## Contribution
-      ---
-      _If you would like to contribute, please adhere to these guidelines:_
-      ${data.contribution}`
-  };
-
-  //testing
-  if (data.testing) {
-    markdownTemplate +=
-      `## Testing
-      ---
-      _Instructions for testing application:_
-      ${data.testing}`
-  };
-
-  //questions
-  if (data.questions) {
-    markdownTemplate +=
-      `## Questions
-      ---
-      _For further questions please follow these steps:_
-      ${data.questions}`
-  }
-
-  //contact
-  markdownTemplate +=
-    `## Contact
-      ---
-      _Contact Info:_
-      * GitHub: [${data.username}](https://github.com/${data.username})
-      * Email: [${data.email}](${data.email})`;
-  
   //license
   let licenseOption = `${data.license}`;
   let licenseLink = '';
   //if conditional to give the license the correct link
   if (licenseOption === 'GNU AGPLv3') {
+    licenseOption = 'GNUAGPLv3';
     licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
   };
   if (licenseOption === 'GNU GPLv3') {
+    licenseOption = 'GNUGPLv3';
     licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
   };
   if (licenseOption === 'GNU LGPLv3') {
+    licenseOption = 'GNULGPLv3';
     licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
   };
   if (licenseOption === 'Mozilla Public License 2.0') {
+    licenseOption = 'MozillaPublicLicense2.0';
     licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
   };
   if (licenseOption === 'Apache License 2.0') {
+    licenseOption = 'ApacheLicense2.0';
     licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
   };
   if (licenseOption === 'MIT License') {
+    licenseOption = 'MITLicense';
     licenseLink = 'https://choosealicense.com/licenses/mit/';
   };
   if (licenseOption === 'Boost Software License 1.0') {
+    licenseOption = 'BoostSoftwareLicense1.0';
     licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
   };
   if (licenseOption === 'The Unlicense') {
+    licenseOption = 'TheUnlicense';
     licenseLink = 'https://choosealicense.com/licenses/unlicense/';
   };
 
+//create variable to hold markdown template and dynamically add to it
+let markdownTemplate =
+    
+//title, badge, and description
+    
+`# ${data.title}
+
+## Description
+
+${data.description}
+
+![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
+
+You can access more badges and their purposes at [shields.io](https://shields.io)
+
+`;
+  //create optional sections in a table of contents to 
+  //be added after required title, description
+
+let tableOfContents =
+`## Table of Contents`;
+  if (data.installation) {
+    tableOfContents +=
+      `
+  * [Installation](#installation)`
+  };
+  if (data.instructions) {
+    tableOfContents +=
+      `
+  * [Usage](#usage)`
+  };
+  if (data.contribution) {
+    tableOfContents +=
+      `
+  * [Contribution](#contribution)`
+  };
+  if (data.testing) {
+    tableOfContents +=
+      `
+  * [Testing](#testing)`
+  };
+  if (data.questions) {
+    tableOfContents +=
+      `
+  * [Questions](#questions)`
+  };
+
+  //append table to template
+  markdownTemplate += tableOfContents;
+
+  //add contact info and license (required)
   markdownTemplate +=
-    `## License
-      ---
-      This application has the ${licenseOption}. 
-      For more information please view the [license description](${licenseLink}).`;
+    `
+  * [Contact](#contact)`;
+  markdownTemplate +=
+    `
+  * [License](#license)
+    
+    `;
+
+  //add optional sections content
+
+  //installation
+  if (data.installation) {
+    markdownTemplate +=
+      `
+## Installation
+    
+  _Follow these steps to properly install this application:_
+
+  ${data.installation}`
+  };
+
+  //usage
+  if (data.instructions) {
+    markdownTemplate +=
+      `
+      
+## Usage
+
+  _Instructions for use:_
+
+  ${data.instructions}`
+  };
+
+  //contributions
+  if (data.contribution) {
+    markdownTemplate +=
+      `
+      
+## Contribution
+
+  _If you would like to contribute, please adhere to these guidelines:_
+
+  ${data.contribution}`
+  };
+
+  //testing
+  if (data.testing) {
+    markdownTemplate +=
+      `
+      
+## Testing
+
+  _Instructions for testing application:_
+
+  ${data.testing}`
+  };
+
+  //questions
+  if (data.questions) {
+    markdownTemplate +=
+      `
+      
+## Questions
+      
+  _For further questions please follow these steps:_
+
+  ${data.questions}`
+  }
+
+  //contact
+  markdownTemplate +=
+    `
+    
+## Contact
+      
+  _Contact Info:_
+
+  * GitHub: [${data.username}](https://github.com/${data.username})
+
+  * Email: [${data.email}](${data.email})`;
+
+  
+  markdownTemplate +=
+    `
+    
+## License
+
+      
+  _This application has the ${data.license}._
+      
+  For more information please view the [license description](${licenseLink}).
+  
+  `;
   return markdownTemplate;
 }
 
